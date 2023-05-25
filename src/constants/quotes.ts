feca24c851e7dbb8126668bccfac4096e5ca0292
Estarlin R <estarlincito@Estarlincito-2.local>
2023-05-25 19:50:27 -0400

    add new tag

--- src/constants/quotes.ts
+++ src/constants/quotes.ts
@@ -2,6 +2,7 @@
 export const optiontags = [
   { value: 'education', label: 'education' },
   { value: 'psychological', label: 'psychological' },
+  { value: 'motivational', label: 'motivational' },
   { value: 'tech', label: 'tech' },
   { value: 'ui', label: 'ui' },
   { value: 'work', label: 'work' },
