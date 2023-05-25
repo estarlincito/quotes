3e8e4d800e22d7b5884ed05431fb37514aa7fdd7
Estarlin R <estarlincito@Estarlincito-2.local>
2023-05-25 08:13:50 -0400

    try to check if cookie in ios shortcut is correct

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -4,3 +4,7 @@ import { NextResponse } from 'next/server';
 export const POST = async (req: Request) => {
   return NextResponse.json(await newQuote(req));
 };
+
+export const GET = async (req: Request) => {
+  return new Response("it's okay");
+};
