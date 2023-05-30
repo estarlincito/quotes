39a46c5183d1303e7f2717aea13ea85e56d6e769
Estarlin R <estarlincito@Estarlincito-2.local>
2023-05-30 15:49:19 -0400

    optimizing formquotes 3.0

--- src/components/options.tsx
+++ src/components/options.tsx
@@ -1,6 +1,6 @@
 import { Name, Values } from '@/types/values';
 import clsx from 'clsx';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 interface OptionsProps {
   items: string[];
@@ -10,24 +10,67 @@ interface OptionsProps {
 }
 
 const Options: FC<OptionsProps> = ({ items, handleSelec, values, name }) => {
-  //const _items = items.filter((item) => item.includes(values[name]));
-  const [data, setData] = useState(
-    items.filter((item) => item.includes(values[name]))
+  //   const [data, setData] = useState(
+  //     items.filter((item) => item.includes(values[name]))
+  //   );
+
+  const data = items.filter(
+    (item) =>
+      !item.includes(
+        name === 'author' ? values[name] : values[name].replaceAll(' ', '')
+      )
   );
+  const [active, setActive] = useState(false);
 
-  //console.log(values[name].endsWith(' '));
+  useEffect(() => {
+    //if input value change
+    if (values[name].length > 0) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
 
-  //console.log(data.indexOf(values[name].replace(' ', '')));
+    //if is author turn off active
+    if (
+      (name === 'author' && data.includes(values[name])) ||
+      (values[name].endsWith(' ') && values[name].length > 4)
+    ) {
+      setActive(false);
+    }
 
-  console.log(data.filter((item) => item !== values[name].replace(' ', '')));
-  //   useEffect(() => {
-  //     if (data.includes(values[name])) {
-  //       console.log(data.indexOf(values[name]));
-  //       //setData([...data]);
-  //     }
-  //   }, [values]);
+    //if tags
+    if (name === 'tags' && values[name].endsWith(' ')) {
+      setActive(true);
+    }
+    // if (name === 'author') {
+    //   //   const authors = items.filter((item) => item.includes(values[name]));
+    //   //   setData(authors);
+    //   //   //if tags input end with space
+    //   //   if (values[name].endsWith(' ')) {
+    //   //     if (values[name].length > 4) {
+    //   //       setActive(false);
+    //   //     } else {
+    //   //       setActive(true);
+    //   //     }
+    //   //   }
+    //   //   //if author input have some authors value
+    //   //   if (values[name] !== ' ' || authors.includes(values[name])) {
+    //   //     setActive(false);
+    //   //   }
+    // }
+    // if (name === 'tags') {
+    //   //   const tags = items.filter((item) =>
+    //   //     item.includes(values[name].replace(' ', ''))
+    //   //   );
+    //   //   setData(tags);
+    //   //   //if author input have some authors value
+    //   //   if (tags.includes(values[name])) {
+    //   //     setActive(false);
+    //   //   }
+    // }
+  }, [data, items, name, values]);
 
-  return values[name] === '' || data.includes(values[name]) ? null : (
+  return active === false ? null : (
     <ul
       className={clsx(
         'flex flex-col',
