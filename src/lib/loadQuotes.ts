32864643c8b6965f71a89a5aef961840aa9a3678
Estarlin R <estarlincito@Estarlincito-2.local>
2023-09-22 03:04:15 -0400

    disable no-store

--- src/lib/loadQuotes.ts
+++ src/lib/loadQuotes.ts
@@ -2,8 +2,8 @@ import { Quotes } from '@/types/quotes';
 
 const loadQuotes = async () => {
   const res = await fetch(
-    'https://raw.githubusercontent.com/estarlincito/iDB/quotes/data_en.json',
-    { cache: 'no-store' }
+    'https://raw.githubusercontent.com/estarlincito/iDB/quotes/data_en.json'
+    // { cache: 'no-store' }
   );
   const quotes = await res.json();
   return quotes as Quotes[];
